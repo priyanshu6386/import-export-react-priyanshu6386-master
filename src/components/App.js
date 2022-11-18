26623e721db4325c7c47c26f2817c5c902bd3e64
priyanshu6386 <priyanshuv624@gmail.com>
2022-11-18 19:28:02 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Heading from "./Heading";
+import InputQuery from "./InputQuery";
+import SubHeading from "./SubHeading"
+import SubmitButton from "./SubmitButton"
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main">
+      < Heading/>
+      < InputQuery/>
+      < SubHeading/>
+      < SubmitButton/>
+    </div>
+  )
+}
+
+
